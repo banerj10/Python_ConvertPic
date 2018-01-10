add9deb82d3d33bd62b459eefcbfd8e68e4850dd
Anirban <banerj10@illinois.edu>
2018-01-10 15:41:25 +0700

    Initial functionality complete

--- ConvertPic.py
+++ ConvertPic.py
@@ -1,45 +1,46 @@
-import tkinter
+import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
+import os
 
 class ConvertPic:
 
     def __init__(self, master):
 
             # frame widget created as container for other widgets
-        frame = Frame(master)
+        frame = tk.Frame(master)
             # frame uses grid geometry, N+E+W+S sticky makes frame expand
             # on all sides when window is resized
-        frame.grid(sticky=N+E+W+S)
+        frame.grid(sticky=tk.N+tk.E+tk.W+tk.S)
 
             # define and initialize variables for calculator class
             # file_list contains list of files to be converted 
         self.file_list = []      
 
             # display listing pictures to be converted as a label widget
-        self.display = Label(
-            frame, text="", font=("Verdana", 10), anchor=NW, justify=LEFT, \
+        self.display = tk.Label(
+            frame, text="", font=("Verdana", 10), anchor=tk.NW, justify=tk.LEFT, \
             fg="black", bg="white")
             # position calculator display using grid geometry
         self.display.grid(row=0, column=0, columnspan=2, rowspan=5, \
-            padx=3, pady=3, sticky=N+E+W+S)
+            padx=3, pady=3, sticky=tk.N+tk.E+tk.W+tk.S)
       
             # calculator buttons defined as button widgets
             # note that the command (for button press) uses lambda functions
             # this is required to ensure evaluation only happens during
             # runtime when the corresponding button is pressed
 
-        self.add = Button(frame, text="Add Pictures", command=lambda: self.press_add())
-        self.add.grid(row=5, column=0, padx=1, pady=1, sticky=N+E+W+S)
-        self.run = Button(frame, text="Run PicConvert", command=lambda: self.press_run())
-        self.run.grid(row=5, column=1, padx=1, pady=1, sticky=N+E+W+S)
+        self.add = tk.Button(frame, text="Add Pictures", command=lambda: self.press_add())
+        self.add.grid(row=5, column=0, padx=1, pady=1, sticky=tk.N+tk.E+tk.W+tk.S)
+        self.run = tk.Button(frame, text="Run PicConvert", command=lambda: self.press_run())
+        self.run.grid(row=5, column=1, padx=1, pady=1, sticky=tk.N+tk.E+tk.W+tk.S)
 
         # gives each element in frame a non-zero weight in order
         # to allow proper resizing if window is resized
         for p in range(2):
-            Grid.columnconfigure(frame, p, weight=1)
+            tk.Grid.columnconfigure(frame, p, weight=1)
         for q in range(5):
-            Grid.rowconfigure(frame, q, weight=1)
+            tk.Grid.rowconfigure(frame, q, weight=1)
 
         # method for adding pictures to the conversion list
     def press_add(self):
@@ -57,18 +58,30 @@ class ConvertPic:
         # method for running the picture conversion option
     def press_run(self):
         for f in self.file_list:
-            img = Image.open(f)
+            head, tail = os.path.split(f)
+            directory = os.path.dirname(head + "/ConvertPic/")
+            if not os.path.exists(directory):
+                os.mkdir(directory)
+            new_pth = head + "/ConvertPic/" + tail
+            root, _ = os.path.splitext(new_pth)
+            fout = root + ".jpg"
+            try:
+                img = Image.open(f)
+                rgb_img = img.convert("RGB")
+                rgb_img.save(fout)
+            except IOError:
+                print("error in conversion")
 
 if __name__ == "__main__":
     
         # create root window
-    root = Tk()
+    root = tk.Tk()
     root.wm_title("Picture Converter")
         # assign weight root grid (not really needed in this case)
-    Grid.columnconfigure(root, 0, weight=1)
-    Grid.rowconfigure(root, 0, weight=1)
+    tk.Grid.columnconfigure(root, 0, weight=1)
+    tk.Grid.rowconfigure(root, 0, weight=1)
         # create calculator app as child of root
-    calc = ConvertPic(root)
+    conv = ConvertPic(root)
 
     root.update()
         # ensures calculator is centered on screen when opened
